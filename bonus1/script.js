c7a8c895afc54fd77654bf54b9797b297df74037
alexcapoluongo <alexcapoluongo@icloud.com>
2022-04-12 10:15:02 +0200

    const rowWrapper moved  outside the operation

--- bonus1/script.js
+++ bonus1/script.js
@@ -1,4 +1,5 @@
 // -stampare nella console numeri da 1 a 100
+const rowWrapper = document.querySelector('.row');
 for (let i = 1; i <= 100; i++) {
     // 1. definire se è multiplo di 3 o 5 o entrambi
     let fizzOrBuzz;
@@ -17,7 +18,6 @@ for (let i = 1; i <= 100; i++) {
     
     // 3.aggiungere elementi in dom con i
 
-    const rowWrapper = document.querySelector('.row');
     rowWrapper.innerHTML += `<div class= "box ${fizzOrBuzz}">${fizzOrBuzz}</div>`;
 }
 
